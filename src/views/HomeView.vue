cb2eed3995746ffac32acff20854e0408d6e6ecd
izacpeterson <izacpeterson@gmail.com>
2022-02-20 22:29:25 -0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Weather from "../components/Weather.vue";
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <h2>Quick Links</h2>
+    <ul>
+      <a href="">Email</a>
+      <a href="">Canvas</a>
+      <a href="">Skyward</a>
+    </ul>
+    <h2>Weather</h2>
+    <Weather></Weather>
+  </main>
+</template>
+
+<style scoped>
+ul {
+  display: flex;
+  flex-direction: column;
+}
+</style>
